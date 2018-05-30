8691d5470794050cc80d8df6ce2cae99cdf32fd5
thedon122 <donovan122@yahoo.com>
2018-05-30 17:14:58 -0400

    created first route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    def index
+        @user = User.all
+    end
+    def create
+        @users =User.create!(users_params)
+    end
+
+    private
+    def users_params
+        params.require(:user).permit(:name, :birth_year, :gender, :species)
+    end
 end
