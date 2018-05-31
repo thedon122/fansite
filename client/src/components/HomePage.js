2e4852385935ef3f12b91b9f91816bcbe0bcb735
thedon122 <donovan122@yahoo.com>
2018-05-31 17:55:58 -0400

    imported styled component

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
+import styled from 'styled-components';
 
+const Header = styled.div`
+
+`
 class HomePage extends Component {
     render() {
         return (
             <div>
+
             </div>
                 )
             }
