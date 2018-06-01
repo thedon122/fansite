b27dd08b7f87761001914844ebed779b675da7ca
thedon122 <donovan122@yahoo.com>
2018-06-01 11:21:55 -0400

    created form page

--- client/src/components/NewUserForm.js
+++ client/src/components/NewUserForm.js
@@ -1,4 +1,42 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components';
-import { SearchForm } from 'react-materialize'
+import { Form } from 'react-materialize'
 import axios from 'axios'
+
+const Header = styled.div`
+padding: 20px;
+margin: auto;
+text-align: center;`
+const FormBody = styled.div`
+padding: 40px;
+margin: auto;
+text-align: center;
+`
+class NewUserForm extends Component {
+    state = {
+        user: {
+            name: '',
+            username: '',
+            password: ''
+        }
+    }
+    handleChange = (event) => {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        console.log(user)
+        this.setState({ user })
+      }
+    
+    render() {
+        return (
+            <div>
+                <Header><h2>New Post Form</h2></Header>
+                <Row>
+                <Input onChange={this.handleChange} s={8} label="Name" />
+                <Input onChange={this.handleChange} s={8} label="User Name" />
+                <Input onChange={this.handleChange} s={8} label="Password" />
+                </Row>
+            </div>
+        )
+    }
+}
