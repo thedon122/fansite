d2099cf6aa7372217c48aa959d328466eba97d3b
thedon122 <donovan122@yahoo.com>
2018-06-01 16:18:12 -0400

    working one back in

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ApplicationRecord
-    has_and_belongs_to_many :User
+    has_many :favorites, dependent: :destroy
+    has_many :users, through: :favorite
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    has_many :Person, dependent: :destroy
+    has_many :favorite, dependent: :destroy
+    has_many :Person, through: :favorite
     # has_many :Planet, dependent: :destroy
     # has_many :Starship, dependent: :destroy
     # has_many :Vehicle, dependent: :destroy
